eacf21722851e4448aa6495b51976136230a5584
angelairena14 <angela.irena@koinworks.com>
2021-01-15 18:56:07 +0700

    remove unused code

--- app/src/main/java/com/timeromannews/util/HeaderInterceptor.kt
+++ app/src/main/java/com/timeromannews/util/HeaderInterceptor.kt
@@ -51,15 +51,6 @@ class HeaderInterceptor : Interceptor {
                                 )
                             )
                         }
-                    } else {
-                        RxBus.publish(
-                            RetrofitResponse(
-                                response.code,
-                                if (obj.has(MESSAGE)) obj.getString(MESSAGE) else "",
-                                "",
-                                ""
-                            )
-                        )
                     }
                 }
             } catch (e: JSONException) {
